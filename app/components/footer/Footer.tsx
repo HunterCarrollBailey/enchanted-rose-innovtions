6032392bec7379950fcb222c75fac6417e3b606f
Bailey Carroll <bailey@enchantedroseinnovations.com>
2024-08-15 16:39:47 -0700

    Code formatting adjustments

--- app/components/footer/Footer.tsx
+++ app/components/footer/Footer.tsx
@@ -1,9 +1,11 @@
 const Footer = () => {
-    return (
-        <footer className="text-slate-200 bg-slate-800/70 backdrop-blur shadow-inner shadow-cyan-400 rounded-t-2xl p-3">
-            <h3 className="text-lg text-center"><span>&copy; </span>Enchanted Rose Innovations | Hunter Bailey</h3>
-        </footer>
-    );
-}
+  return (
+    <footer className="rounded-t-2xl bg-slate-800/70 p-3 text-slate-200 shadow-inner shadow-cyan-400 backdrop-blur">
+      <h3 className="text-center text-lg">
+        <span>&copy; </span>Enchanted Rose Innovations | Hunter Bailey
+      </h3>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
