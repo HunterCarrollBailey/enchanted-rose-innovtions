e6892d84b22910be7cc96fb9decb0b72b8176f4c
Bailey Carroll <bailey@enchantedroseinnovations.com>
2024-08-15 16:40:05 -0700

    Changed the shadow from outer to inner, for a nice 3d effect

--- app/components/card/Card.tsx
+++ app/components/card/Card.tsx
@@ -1,14 +1,17 @@
-import React from "react"
+import React from "react";
 interface CardProps {
-    id? : string | string[];
-    adlClasses? : string | string[];
-    children : React.ReactNode;
+  id?: string;
+  adlClasses?: string | string[];
+  children: React.ReactNode;
 }
-const Card : React.FC<CardProps> = ({id, adlClasses, children }) => {
-    return (
-        <div id={id} className={`bg-slate-800/70 backdrop-blur-sm p-5 rounded-2xl shadow-md shadow-cyan-400 border border-cyan-400 ${adlClasses}`}>
-            {children}
-        </div>
-    );
-}
+const Card: React.FC<CardProps> = ({ id, adlClasses, children }) => {
+  return (
+    <div
+      id={id}
+      className={`rounded-2xl bg-slate-800/70 p-5 shadow-inner shadow-cyan-400 backdrop-blur-sm ${adlClasses}`}
+    >
+      {children}
+    </div>
+  );
+};
 export default Card;
