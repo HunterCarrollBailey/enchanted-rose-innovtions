3ca10adc6b61a32e0e7a8e27df65289edfaea53f
Bailey Carroll <bailey@enchantedroseinnovations.com>
2024-08-15 17:00:13 -0700

    Active & hover styling done & desktop view done.

--- app/components/header/NavBarLink.tsx
+++ app/components/header/NavBarLink.tsx
@@ -1,15 +1,29 @@
+"use client";
 import BassComponentProps from "@/app/components/BaseComponent";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
 interface NavBarLinkProps extends BassComponentProps {
-    href: string;
-    text: string;
-    toggle?: () => void;
+  href: string;
+  text: string;
+  toggle?: () => void;
 }
+
 const NavBarLink = (props: NavBarLinkProps) => {
-    return (
-        <li className="m-2 py-2 px-5 rounded-2xl hover:bg-slate-700">
-            <Link href={props.href} onClick={props.toggle}>{props.text}</Link>
-        </li>
-    )
-}
+  const pathname = usePathname();
+  return (
+    <li className="group m-2 flex flex-col rounded-2xl px-5 py-2 hover:bg-slate-700 md:hover:bg-transparent">
+      <Link
+        href={props.href}
+        onClick={props.toggle}
+        className={`${pathname == props.href ? "font-bold text-cyan-400" : ""}`}
+      >
+        {props.text}
+      </Link>
+      <div
+        className={`h-1 w-0 rounded-full bg-cyan-400 transition-all duration-700 ease-in-out group-hover:w-full`}
+      ></div>
+    </li>
+  );
+};
 export default NavBarLink;
--- app/components/header/Navbar.tsx
+++ app/components/header/Navbar.tsx
@@ -13,14 +13,18 @@ const Navbar = (props: NavBarProps) => {
   const toggleNav = () => setNavOpen(!navOpen);
   return (
     <nav
-      className={`sticky top-0 w-full rounded-b-2xl bg-slate-800/70 p-4 text-slate-200 shadow-md shadow-cyan-400 backdrop-blur`}
+      className={`sticky top-0 flex w-full flex-col rounded-b-2xl bg-slate-800/70 p-4 text-slate-200 shadow-md shadow-cyan-400 backdrop-blur md:flex-row md:py-0`}
     >
       <div className="flex items-center justify-around">
         <h1 className="text-xl font-bold text-slate-200">{props.brand}</h1>
-        <FaBars className="text-xl" role="button" onClick={toggleNav} />
+        <FaBars
+          className="text-xl md:invisible"
+          role="button"
+          onClick={toggleNav}
+        />
       </div>
       <ul
-        className={`flex w-full flex-col justify-start overflow-hidden transition-all duration-700 ease-in ${navOpen ? "max-h-screen opacity-100" : "max-h-0 opacity-0"}`}
+        className={`flex w-full flex-col justify-start overflow-hidden transition-all duration-700 ease-in md:ml-auto md:w-auto md:flex-row ${navOpen ? "max-h-screen opacity-100 md:max-h-fit md:opacity-100" : "max-h-0 opacity-0 md:max-h-fit md:opacity-100"}`}
       >
         <NavBarLink href={"/"} text={"Home"} toggle={toggleNav} />
         <NavBarLink href={"/about"} text={"About"} toggle={toggleNav} />
