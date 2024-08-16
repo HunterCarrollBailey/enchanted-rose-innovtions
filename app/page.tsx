40062c8d15c16e493ddf1cd63334acbba584fcc6
Bailey Carroll <bailey@enchantedroseinnovations.com>
2024-08-15 17:22:37 -0700

    Using the new components for the Hero and AboutBailey, and added some comments on plans.

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,17 @@
-import Card from "@/app/components/card/Card";
-import Link from "next/link";
+// Home page, has a small component about Bailey, the hero and call to action, and then some recent blog posts.
+import Hero from "@/app/components/homePage/Hero";
+import AboutBailey from "@/app/components/homePage/AboutBailey";
 
 export default function Home() {
   return (
-    <main className={`my-5`}>
-      <Card adlClasses={`flex flex-col items-center align-center my-5`}>
-        <h1 className={`text-center text-3xl font-bold text-slate-200`}>
-          eLearning Re-Imagined
-        </h1>
-        <h2 className={`my-2 text-center text-xl font-semibold text-slate-200`}>
-          A new kind of LMS, focusing on the what matters most, the students.
-        </h2>
-        <button
-          className={`ml-auto mt-2 rounded-2xl bg-slate-600/70 px-5 py-2 text-slate-200 shadow-md shadow-cyan-400 transition-all ease-in hover:-translate-y-1 hover:shadow-lg hover:shadow-cyan-400`}
-        >
-          <Link href={`/acolyte`}>Learn More</Link>
-        </button>
-      </Card>
-      <Card>
-        <h1 className={`text-center text-3xl font-bold text-slate-200`}>
-          About The Creator
-        </h1>
-        <p className="my-2 text-justify leading-relaxed text-slate-200">
-          Hiya! My name{`'`}s Bailey, Acolyte started out as an idea written on
-          a napkin at my kitchen table in May of 2018. Since then it has become
-          my larges and most ambitious project. I specialized in being a
-          Technical Problem Solver and the issue my friend faced during that
-          first conversation was the spark that started it all. If you would
-          like to know more about me please checkout my portfolio at{" "}
-          <Link
-            href={`https://hunterbailey.dev`}
-            target={"_blank"}
-            className={`text-cyan-400 underline`}
-          >
-            hunterbailey.dev
-          </Link>
-        </p>
-      </Card>
+    <main
+      className={`my-5 flex h-fit flex-col md:flex-row md:items-stretch md:gap-5`}
+    >
+      <Hero />
+      <AboutBailey />
+      <div className={`:md:flex-row flex flex-col md:gap-5`}>
+        {/* Blog Posts / Project Mentions / Etc will go here. This will wait until I have some content done. */}
+      </div>
     </main>
   );
 }
